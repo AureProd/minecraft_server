f9c8ca61f093d173eba539aead127387d3e62559
JB <jean-baptiste.huet@epitech.eu>
2022-12-01 11:53:24 +0100

    update server infra

--- src/start.sh
+++ src/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-screen -dm -S server -L -Logfile /srv/logs/server.log bash -c "cd /srv/server; java -Xms$SERVER_MIN_RAM -Xmx$SERVER_MAX_RAM -jar /srv/spigot.jar nogui"
+screen -dm -S server -L -Logfile /srv/logs/server.log bash -c "cd /srv/server; ./run.sh"
 
 screen -S server -X colon "logfile flush 0^M"

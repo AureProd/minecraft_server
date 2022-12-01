2e1cf328dbad9e8bf92f1f68bb9ebeae336ddba0
JB <jean-baptiste.huet@epitech.eu>
2022-12-01 09:16:45 +0100

    update scripts

--- down.sh
+++ down.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker-compose --project-name spigot-server -f docker-compose.yml --env-file .env down
+if [ ! -f .env ]; then
+    echo "No .env file found. Please create one."
+    exit 1
+else
+    $INSTANCE_NAME=$(grep INSTANCE_NAME .env | cut -d '=' -f2)
+fi
+
+docker-compose --project-name server-$INSTANCE_NAME -f docker-compose.yml --env-file .env down
